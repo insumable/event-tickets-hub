692550ee91e6e7ad4a806f8972076565f7ff9bbf
insumable <patrasoham2006@gmail.com>
2026-07-03 09:58:45 +0530

    repo layer directory

---
+++ tickets/src/main/java/com/insumable/tickets/repositories/QrCodeRepository.java
@@ -0,0 +1,15 @@
+package com.insumable.tickets.repositories;
+
+import com.insumable.tickets.domain.entities.QrCode;
+import com.insumable.tickets.domain.enums.QrCodeStatusEnum;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface QrCodeRepository extends JpaRepository<QrCode, UUID> {
+    Optional<QrCode> findByTicketIdAndTicketPurchaserId(UUID ticketId, UUID ticketPurchaseId);
+    Optional<QrCode> findByIdAndStatus(UUID id, QrCodeStatusEnum status);
+}
---
+++ tickets/src/main/java/com/insumable/tickets/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.insumable.tickets.repositories;
+
+import com.insumable.tickets.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
